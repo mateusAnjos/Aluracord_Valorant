4c2c3c195dddc3a3c0535e5bc4e0a85f043aa592
mateusAnjos <mateus.anjosdefaria@gmail.com>
2022-01-27 13:57:58 -0300

    added MessageList and Header into Chat, deleted external files

--- pages/chat.js
+++ pages/chat.js
@@ -1,8 +1,8 @@
-import { Box, Text, TextField, Image, } from '@skynexui/components';
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
-import Header from './header'
-import MessageList from './messagelist'
+import {useRouter} from "next/router"
+
 
 
 
@@ -106,4 +106,111 @@ export default function Chat() {
         </Box>
     )
 }
+function Header() {
 
+    const route = useRouter()
+
+    return (
+        <>
+            <Box styleSheet={{ 
+                width: '100%', 
+                marginBottom: '16px', 
+                display: 'flex', 
+                alignItems: 'center', 
+                justifyContent: 'space-between',
+                color: appConfig.theme.colors.primary[400],
+                }} >
+                <Text variant='heading3'>
+                    Mission Log
+                </Text>
+                <Button
+                buttonColors={{
+                    contrastColor: appConfig.theme.colors.neutrals["000"],
+                    mainColor: appConfig.theme.colors.primary[400],
+                    mainColorLight: appConfig.theme.colors.primary[400],
+                    mainColorStrong: appConfig.theme.colors.primary[600],
+                  }}
+                    onClick={function (){
+                        //Decidir qual página vai aparecer quando submeter
+                        route.push('')
+                    }}
+                    variant='secondary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+
+function MessageList(props) {
+    console.log(props.messageList);
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflow: 'auto',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+                
+            }}
+        >
+            {props.messageList.map((message) => {
+                return (
+                    <Text
+                        key={message.id}
+                        tag="li"
+                        styleSheet={{
+                            wordBreak: 'break-word',
+                            borderRadius: '5px',
+                            padding: '6px',
+                            marginBottom: '12px',
+                            hover: {
+                                backgroundColor: appConfig.theme.colors.neutrals[700],
+                            }
+                        }}
+                    >
+                        <Box
+                            styleSheet={{
+                                marginBottom: '8px',
+                            }}
+                        >
+                            <Image
+                                styleSheet={{
+                                    width: '20px',
+                                    height: '20px',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    marginRight: '8px',
+                                }}
+                                src={`https://github.com/${message.from}.png`}
+                            />
+                            <Text tag="strong"
+                                styleSheet={{
+                                    color: appConfig.theme.colors.primary[400]
+                                }}>
+                                {message.from}
+                            </Text>
+                            <Text
+                                styleSheet={{
+                                    fontSize: '10px',
+                                    marginLeft: '8px',
+                                    color: appConfig.theme.colors.neutrals[300],
+                                }}
+                                tag="span"
+                            >
+                                {(new Date().toLocaleString())}
+                            </Text>
+                        </Box>
+                        {message.text}
+                    </Text>
+                )
+            })}
+
+        </Box>
+    )
+}
